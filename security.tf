99df348755ec599d611fb4012c36ebacbd415c20
jagadamma <96439002+jagadamma@users.noreply.github.com>
2023-06-06 13:19:08 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,15 +1,21 @@
 resource "aws_security_group" "sq" {
  name        = "sq"
- description = "Allow HTTPS to web server"
+ description = "Allow ssh & all traffic to web server"
  vpc_id      = aws_vpc.vpc.id
+ 
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
-   description = "HTTPS ingress"
-   from_port   = 0
-   to_port     = 65535
-   protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
- }
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 egress {
    from_port   = 0
